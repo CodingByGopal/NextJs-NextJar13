485098ba40e0f482daef24e331e33a32a4b87223
CodingByGopal <gopalji028@gmail.com>
2022-12-04 19:26:19 +0530

    image loading issue fixed in SSR

--- components/dataFetcher.js
+++ components/dataFetcher.js
@@ -1,5 +1,6 @@
 const fetchData = async (url, option) => {
   const response = await fetch(url, option);
+
   if (!response.ok) {
     // This will activate the closest `error.js` Error Boundary
     throw new Error("Failed to fetch data");
--- next.config.js
+++ next.config.js
@@ -11,6 +11,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "image.tmdb.org",
       },
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+      },
     ],
   },
 };
